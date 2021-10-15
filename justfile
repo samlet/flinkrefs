ff7df3ab8ed3706e2c2673401e5092a9edeaf3e8
Samlet Wu <xiaofei.wu@gmail.com>
2021-10-15 19:06:17 +0800

    add kafka sink

--- justfile
+++ justfile
@@ -1,3 +1,21 @@
+base:
+    foreman start -f Procfile_base
+srv:
+    foreman start -f Procfile_services
+
+topics:
+    kafka-topics --list --bootstrap-server localhost:9092
+desc topic:
+    kafka-topics --zookeeper localhost:2181 --topic {{topic}} --describe
+create topic:
+    kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic {{topic}}
+list topic:
+    kafka-console-consumer --bootstrap-server localhost:9092 --topic {{topic}} --from-beginning
+consume topic:
+    kafka-console-consumer --bootstrap-server localhost:9092 --topic {{topic}}
+produce topic:
+    kafka-console-producer --broker-list localhost:9092 --topic {{topic}}
+
 fraud:
 	mvn compile exec:java -Dexec.mainClass="com.bluecc.refs.FraudDetectionJob"
 run program:
---
+++ src/main/java/com/bluecc/refs/sink/SinkKafka.java
@@ -0,0 +1,47 @@
+package com.bluecc.refs.sink;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+
+import java.util.Properties;
+
+/**
+ * $ just create sensor && just create sinktest
+ * $ just consume sinktest
+ * $ just produce sensor
+ * >sensor_1,1547718199,35.8
+ * >sensor_1,1547718212,37.1
+ */
+public class SinkKafka {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+//        // 从文件读取数据
+//        DataStream<String> inputStream = env.readTextFile("dataset/sensor.txt");
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        // 从文件读取数据
+        DataStream<String> inputStream = env.addSource( new FlinkKafkaConsumer<String>("sensor", new SimpleStringSchema(), properties));
+
+        // 转换成SensorReading类型
+        DataStream<String> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2])).toString();
+        });
+
+        dataStream.addSink( new FlinkKafkaProducer<String>("localhost:9092", "sinktest", new SimpleStringSchema()));
+
+        env.execute();
+    }
+}
